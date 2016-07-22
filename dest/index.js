38082447ec38c713e94f2d7a05213065fd7e987e
MNB <buyskih@gmail.com>
2016-07-22 10:24:30 +0300

    $save, $update, $remove methods

--- dest/index.js
+++ dest/index.js
@@ -1,8 +1,13 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 System.register("index", ['angular'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var angular_1;
-    var NgLocalResource;
+    var NgLocalResourceModel, NgLocalResourceModelAbstract, NgLocalResource;
     function $localResource(localStorageService, $q) {
         return function (key, config) {
             return new NgLocalResource(key, config, localStorageService, $q);
@@ -15,6 +20,34 @@ System.register("index", ['angular'], function(exports_1, context_1) {
                 angular_1 = angular_1_1;
             }],
         execute: function() {
+            NgLocalResourceModel = (function (_super) {
+                __extends(NgLocalResourceModel, _super);
+                function NgLocalResourceModel() {
+                    _super.apply(this, arguments);
+                }
+                return NgLocalResourceModel;
+            }(NgLocalResourceModelAbstract));
+            exports_1("NgLocalResourceModel", NgLocalResourceModel);
+            NgLocalResourceModelAbstract = (function () {
+                function NgLocalResourceModelAbstract() {
+                }
+                NgLocalResourceModelAbstract.prototype.$save = function () {
+                    return this.$resource.save(this);
+                };
+                NgLocalResourceModelAbstract.prototype.$update = function () {
+                    return this.$resource.update(this);
+                };
+                NgLocalResourceModelAbstract.prototype.$remove = function () {
+                    return this.$resource.remove(this[this.$resource.config.pk]);
+                };
+                NgLocalResourceModelAbstract.create = function (object, resource) {
+                    var self = new NgLocalResourceModel();
+                    self.$resource = resource;
+                    angular_1.default.extend(self, object);
+                    return self;
+                };
+                return NgLocalResourceModelAbstract;
+            }());
             NgLocalResource = (function () {
                 function NgLocalResource(key, config, service, $q) {
                     this.key = key;
@@ -29,6 +62,10 @@ System.register("index", ['angular'], function(exports_1, context_1) {
                 NgLocalResource.prototype.save = function (element) {
                     if (element[this.config.pk] === undefined && this.config.createId)
                         element[this.config.pk] = this.createId();
+                    this.service.set(element[this.config.pk], element);
+                    return this.get(element[this.config.pk]);
+                };
+                NgLocalResource.prototype.update = function (element) {
                     this.service.set(element[this.config.pk], element);
                     return this.get(element[this.config.pk]);
                 };
@@ -49,7 +86,9 @@ System.register("index", ['angular'], function(exports_1, context_1) {
                     return q.promise;
                 };
                 NgLocalResource.prototype._get = function (id) {
-                    return this.service.get("" + this.config.prefix + this.key + id);
+                    var value = this.service.get("" + this.config.prefix + this.key + id);
+                    value = NgLocalResourceModelAbstract.create(value, this);
+                    return value;
                 };
                 NgLocalResource.prototype.createId = function () {
                     return this.service.length() + 1;
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,49 @@ import ILocalStorageService = angular.local.storage.ILocalStorageService;
 import IQService = angular.IQService;
 import IPromise = angular.IPromise;
 
-export class NgLocalResource<T> {
-    constructor(private key:string, private config:INgLocalResourceConfig, private service:ILocalStorageService, private $q:IQService) {
+export interface INgLocalResourceModel<T> {
+    $save():IPromise<T>;
+    $update():IPromise<T>;
+    $remove():IPromise<T>;
+}
+
+export interface INgLocalResource<T> {
+    get(id):IPromise<T>;
+    save(element:T):IPromise<T>;
+    update(element:T):IPromise<T>;
+    query():IPromise<T[]>;
+    remove(id):IPromise<T>;
+}
+
+export class NgLocalResourceModel<T> extends NgLocalResourceModelAbstract<T> implements INgLocalResourceModel<T> {
+
+}
+
+abstract class NgLocalResourceModelAbstract<T> {
+    private $resource:NgLocalResource<T>;
+
+    $save():IPromise<T> {
+        return this.$resource.save(this);
+    }
+
+    $update():IPromise<T> {
+        return this.$resource.update(this);
+    }
+
+    $remove():IPromise<T> {
+        return this.$resource.remove(this[this.$resource.config.pk]);
+    }
+
+    static create(object, resource) {
+        let self = new NgLocalResourceModel();
+        self.$resource = resource;
+        angular.extend(self, object);
+        return self;
+    }
+}
+
+export class NgLocalResource<T> implements INgLocalResource<T> {
+    constructor(public key:string, public config:INgLocalResourceConfig, private service:ILocalStorageService, private $q:IQService) {
     }
 
     get(id):IPromise<T> {
@@ -13,9 +54,13 @@ export class NgLocalResource<T> {
     }
 
     save(element:T):IPromise<T> {
-        if (element[this.config.pk] === undefined && this.config.createId)
-            element[this.config.pk] = this.createId();
+        if (element[this.config.pk] === undefined && this.config.createId) element[this.config.pk] = this.createId();
 
+        this.service.set(element[this.config.pk], element);
+        return this.get(element[this.config.pk]);
+    }
+
+    update(element:T):IPromise<T> {
         this.service.set(element[this.config.pk], element);
         return this.get(element[this.config.pk]);
     }
@@ -39,7 +84,9 @@ export class NgLocalResource<T> {
     }
 
     private _get(id:string) {
-        return this.service.get(`${this.config.prefix}${this.key}${id}`);
+        var value = this.service.get(`${this.config.prefix}${this.key}${id}`);
+        value = NgLocalResourceModelAbstract.create(value, this);
+        return value;
     }
 
     private createId():number {
